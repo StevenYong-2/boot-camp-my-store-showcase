34645b96bc2a7e47e86ac5385dfd25510e1094d6
Steven Yong <stevenyong257@gmail.com>
2026-06-05 14:33:06 +0700

    Fix tes2

--- src/app/page.js
+++ src/app/page.js
@@ -1,37 +1,7 @@
-import Link from "next/link";
-
-export default async function HomePage() {
-  const res = await fetch("https://fakestoreapi.com/products", {
-    cache: "no-store",
-  });
-
-  const products = await res.json();
-
+export default function Home() {
   return (
-    <div className="container">
-      <h1>Products</h1>
-
-      <div className="product-grid">
-        {products.slice(0, 5).map((p) => (
-          <div className="card" key={p.id}>
-            <img
-              src={p.image}
-              alt={p.title}
-              className="product-image"
-            />
-
-            <h3>{p.title}</h3>
-
-            <p className="price">${p.price}</p>
-
-            <Link href={`/products/${p.id}`}>
-              <button className="detail-btn">
-                View Details
-              </button>
-            </Link>
-          </div>
-        ))}
-      </div>
+    <div>
+      <h1>Hello World</h1>
     </div>
   );
 }
--- src/app/products/[slug]/page.js
+++ src/app/products/[slug]/page.js
@@ -1,7 +1,47 @@
-export default function Home() {
+"use client";
+
+import { useEffect, useState } from "react";
+import { use } from "react";
+
+export default function Page({ params }) {
+  const { slug } = use(params);
+
+  const [product, setProduct] = useState(null);
+  const [favorite, setFavorite] = useState(false);
+
+  useEffect(() => {
+    fetch(`https://fakestoreapi.com/products/${slug}`)
+      .then((res) => res.json())
+      .then(setProduct);
+  }, [slug]);
+
+  if (!product) return <p className="container">Loading...</p>;
+
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="container">
+      <div className="product-detail">
+
+        {/* LEFT IMAGE */}
+        <img src={product.image} alt={product.title} />
+
+        {/* RIGHT CONTENT */}
+        <div className="product-info">
+
+          <h1>{product.title}</h1>
+
+          <p>{product.description}</p>
+
+          <h2 className="product-price">
+            ${product.price}
+          </h2>
+
+          <button onClick={() => setFavorite(!favorite)}>
+            {favorite ? "❤️ Favorited" : "🤍 Add to Favorite"}
+          </button>
+
+        </div>
+
+      </div>
     </div>
   );
 }
